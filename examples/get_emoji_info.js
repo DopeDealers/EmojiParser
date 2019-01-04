e66edb4f5213dcab62fe3387b59a1874524be6e7
Phil - DopeDealers <dopedealersdev@gmail.com>
2019-01-04 15:58:29 -0500

    Add files via upload

---
+++ examples/get_emoji_info.js
@@ -0,0 +1,14 @@
+/* 
+
+ we will be listing every bit of info of this emoji
+ <o/
+
+*/
+
+
+const Discord = require('discord.js');
+const Emoji = require("emojiparse.js");
+
+const e = Emoji.get("<emojiName:emojiid:>");
+
+console.log(e);
