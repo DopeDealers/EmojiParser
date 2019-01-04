d8dae1ca3d2ca5bb4b318df8e4f0fea9ebaac4b5
Phil - DopeDealers <dopedealersdev@gmail.com>
2019-01-04 14:01:06 -0500

    Add files via upload

---
+++ Readme.md
@@ -0,0 +1,20 @@
+
+## Installation
+
+```sh
+npm i --save emojiparser
+```
+
+## Examples
+`const Discord = require("discord.js");
+ const Emoji = require("emojiparser");
+
+ const e = Emoji(args[0]);
+ 
+ const embed = new Discord.RichEmbed()
+    .setImage(e.url)
+    message.channel.send({
+    embed
+});`
+
+> LiquidBlast Gang https://liquidblast.net
---
+++ emoji.js
@@ -0,0 +1,17 @@
+const Discord = require("discord.js");
+
+module.exports = class emoji {
+
+
+static get(emoji) {
+    if (!emoji) return null;
+    emoji = Discord.Util.parseEmoji(emoji);
+    if(emoji == null) return null;
+    if(emoji.id == null) return null;
+    let type = "";
+    if (emoji.animated) type = "gif";
+    else type = "png";
+    return { url: `https://cdn.discordapp.com/emojis/${emoji.id}.${type}`, id: emoji.id, name: emoji.name, animated: emoji.animated };
+  }
+  // shit code
+};
